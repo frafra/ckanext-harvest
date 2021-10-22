1c7e88cffb7027c426a00e2cb0dd5639d250dd16
Francesco Frassinelli <francesco.frassinelli@nina.no>
2021-10-22 16:58:52 +0200

    Skip protected fields

--- ckanext/harvest/harvesters/base.py
+++ ckanext/harvest/harvesters/base.py
@@ -16,6 +16,7 @@ from ckan import model
 from ckan.model import Session, Package, PACKAGE_NAME_MAX_LENGTH
 
 from ckan.logic.schema import default_create_package_schema
+from ckan.lib.base import config
 from ckan.lib.navl.validators import ignore_missing, ignore
 from ckan.lib.munge import munge_title_to_name, substitute_ascii_equivalents
 
@@ -321,6 +322,9 @@ class HarvesterBase(SingletonPlugin):
                     package_dict.setdefault('name',
                                             existing_package_dict['name'])
 
+                    for field in p.toolkit.aslist(config.get('ckan.harvest.protect_fields')):
+                        if field in existing_package_dict:
+                            package_dict[field] = existing_package_dict[field]
                     new_package = p.toolkit.get_action(
                         'package_update' if package_dict_form == 'package_show'
                         else 'package_update_rest')(context, package_dict)
